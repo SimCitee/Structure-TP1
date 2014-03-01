399e12416a6e1415a12b16e589802f70175cec68
LavaAurora <lavaauroraprojects@gmail.com>
2014-03-01 11:49:58 -0500

    test
    test

--- src/Program.java
+++ src/Program.java
@@ -4,7 +4,6 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
 		Chauffeur jonathan = new Chauffeur("Thivierge", "Jonathan", "1234 adresse", 1986);
 		
 		Interface layout = new Interface();
